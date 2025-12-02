911ae77a62f75b3524b09a7a5d74f84e7d05df93
espenbr <espenbr@dhcp-10-154-201-28.vpn.oracle.com>
2025-12-02 16:01:51 +0100

    fine tuning

--- public/ps1/new-opuociconfigfilefromenv.ps1
+++ public/ps1/new-opuociconfigfilefromenv.ps1
@@ -1,21 +1,31 @@
 <#
 Build OCI config file from env vars: 
 
-'OCI_USER_OCID',
-'OCI_TENANCY_OCID',
-'OCI_FINGERPRINT',
-'OCI_KEY_CONTENT',
-'OCI_REGION'
+Renamed from default used by GitHub OCI inetrgration for cli
+
+            'OCI_USER_OCID',
+            'OCI_TENANCY_OCID',
+            'OCI_FINGERPRINT',
+            'OCI_KEY_CONTENT',
+            'OCI_REGION'
+
+OCI_FINGERPRINT *must* be generated from the file that is loaded into OCI_KEY_CONTENT! 
+
+$env:OCI_KEY_CONTENT = Get-Content -Path '~/.oci/dataops_api_key.pem'
 
 Validate that the _OCID ones are acuatlly ocids 
 
 Create a (temp) file and format as proper OCI config 
 
 Return name of file
+
+Test:
+
+$temp_config = New-OpuOciConfigFileFromEnv
+
+oci iam region list --config-file $temp_config_file
 #>
 function New-OpuOciConfigFileFromEnv {
-    param (
-    )
 
     try {
         ## START: generic section 
@@ -23,7 +33,7 @@ function New-OpuOciConfigFileFromEnv {
         $ErrorActionPreference = "Stop" 
         ## END: generic section
 
-        ## Populate env vars
+        ## Populate env var names
         $ociEnvVarList = @(
             'OCI_USER_OCID',
             'OCI_TENANCY_OCID',
@@ -32,20 +42,41 @@ function New-OpuOciConfigFileFromEnv {
             'OCI_REGION'
         )
 
-        # Ensure all are set
+        ## Ensure all are set
         $loopCnt = 0
         foreach ($ociEnvVar in $ociEnvVarList) {
-            Write-Output "Validating:"
+            Write-Verbose "Validating:"
             try {
                 $envVarValue = (Get-ChildItem Env:$ociEnvVar).Value
-                Write-Output "${ociEnvVar} = ${envVarValue}"
             }
             catch {
                 throw "${ociEnvVar} not set"
             }            
             $loopCnt++
+
         }
 
+        Write-Verbose "Creating temp config file"
+        ## check validity of content
+        Test-OpuOcidString -OcidString $env:OCI_USER_OCID -IsOfType "user"
+        Test-OpuOcidString -OcidString $env:OCI_TENANCY_OCID -IsOfType "tenancy"
+
+        ## TODO: how to handle PEM key in and out of env variable
+        ## Populate temp file for pem key first 
+        $pemKeyFile = New-TemporaryFile
+        Out-File -Append -FilePath $pemKeyFile -InputObject $env:OCI_KEY_CONTENT
+
+        ## Temp config & key
+        $tempFile = New-TemporaryFile
+
+        Out-File -Append -FilePath $tempFile -InputObject "[DEFAULT]" 
+        Out-File -Append -FilePath $tempFile -InputObject "user = ${env:OCI_USER_OCID}" 
+        Out-File -Append -FilePath $tempFile -InputObject "fingerprint = ${env:OCI_FINGERPRINT}" 
+        Out-File -Append -FilePath $tempFile -InputObject "tenancy = ${env:OCI_TENANCY_OCID}" 
+        Out-File -Append -FilePath $tempFile -InputObject "region = ${env:OCI_REGION}" 
+        Out-File -Append -FilePath $tempFile -InputObject "key_file = ${pemKeyFile}" 
+
+        $tempFile.FullName
     }
     catch {
         throw "Test-OpuIpAddr: $_"
